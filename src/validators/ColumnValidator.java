0bb2672563b3993b9984cbd84a960d13daa4d5b8
Mariam-Hussein05 <es.mariam.yamin2023@alexu.edu.eg>
2025-11-28 19:44:47 +0200

    Add ColumnValidator for Sudoku column validation
    Implement column validation for Sudoku boards, allowing validation of a specific column or all columns.

--- src/validators/ColumnValidator.java
+++ src/validators/ColumnValidator.java
@@ -1,5 +1,54 @@
 package validators;
 
-public class ColumnValidator {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author mariam
+ */
+public class ColumnValidator implements SudokuValidator {
+
+        private final Integer columnIndex;//to allow null
+
+    public ColumnValidator() {
+        this.columnIndex = null;
+    }
+
+    public ColumnValidator(int columnIndex) {
+        this.columnIndex = columnIndex;
+    }
+    @Override
+    public boolean validate(int[][] board, List<String> errors) {
+         if (columnIndex != null) {
+            // Validate single column
+            return validateSingleColumn(board, errors, columnIndex);
+        } else {
+            // Validate all columns
+            return validateAllColumns(board, errors);
+        }
+    }
+
+        public boolean validateAllColumns(int[][] board, List<String> errors) {
+        for (int i = 0; i < 9; i++) {
+                if (!validateSingleColumn(board, errors,i)) {
+                    return false;
+                }
+            }
+        return true;
+    }
+
+      public boolean validateSingleColumn(int[][] board, List<String> errors,int columnIndex) {
+        Set<Integer> seen = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            int num = board[i][columnIndex];
+            if (!seen.add(num)) {
+                errors.add("Duplicate number " + num + " found in column " + columnIndex);
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
