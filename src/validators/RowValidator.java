a386d436e152554bf1cc09d548ef730e03464b97
Mariam-Hussein05 <es.mariam.yamin2023@alexu.edu.eg>
2025-11-28 19:43:39 +0200

    Implement row validation methods in RowValidator
    Added methods to validate all rows and single rows in a Sudoku board.

--- src/validators/RowValidator.java
+++ src/validators/RowValidator.java
@@ -4,9 +4,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ *
+ * @author mariam
+ */
+
 public class RowValidator  implements SudokuValidator {
 
-    private final int rowIndex;
+    private final Integer rowIndex;//to allow null
 
     public RowValidator() {
         this.rowIndex = null;
@@ -25,6 +30,27 @@ public class RowValidator  implements SudokuValidator {
             // Validate all rows
             return validateAllRows(board, errors);
         }
+    }
+
+      public boolean validateAllRows(int[][] board, List<String> errors) {
+        for(int i=0 ; i<9; i++){
+             if(!validateSingleRow(board, errors,i)){
+                    return false;
+                }
+        }
+    return true;
+    }
+
+      public boolean validateSingleRow(int[][] board, List<String> errors,int rowIndex) {
+        Set<Integer> seen = new HashSet<>();
+        for (int j = 0; j < 9; j++) {
+            int num = board[rowIndex][j];
+            if (!seen.add(num)) {
+                errors.add("Duplicate number " + num + " found in row " + rowIndex);
+                return false;
+            }
+        }
+        return true;
     }
 
 
