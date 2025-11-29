56b447fe69c63213b76c4d418090b5251b3e0bfc
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-11-29 13:33:48 +0200

    Add files via upload
    added the CLI building logic and used factory method

--- src/app/SudokuApp.java
+++ src/app/SudokuApp.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package app;
-
+import board.SudokuBoard;
+import factory.ValidatorFactory;
+import modes.ModeValidator;
+import utils.CSVReader;
 /**
  *
- * @author AltAWKEl
+ * @author Nour
  */
 public class SudokuApp {
+    
+    public static void main(String[] args) throws Exception {
+        if(args.length!=2)
+        {System.out.println("Invalid command entered, Valid format: java-jar<app-name>.jar<csv filepath> <mode>");}
+        else if(!args[1].trim().equals("0") && !args[1].trim().equals("3") && !args[1].trim().equals("27"))
+        {System.out.println("Invalid mode entered, choose a valid mode: 0,3,27.");}
+        else{String filename=args[0];
+     
+        int mode=Integer.parseInt(args[1]);
+        ValidatorFactory validatorFactory=new ValidatorFactory();
+            ModeValidator ModeValidator = validatorFactory.getType(mode);
+            SudokuBoard board=new SudokuBoard();
+        board.importBoardFromFile(filename);
+        if(board.getBoard()!=null)
+        if(ModeValidator.validate(board.getBoard())) System.out.println("Valid Board.");
+        else System.out.println("Invalid Board.");}
+        
+     
+     
+    }
     
 }
