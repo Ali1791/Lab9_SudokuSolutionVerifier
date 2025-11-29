a6008bb349beb9826d3d47c672d76f1b3cf3e9b2
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-11-29 13:35:02 +0200

    Add files via upload
    includes the importing board from the file logic only

--- src/board/SudokuBoard.java
+++ src/board/SudokuBoard.java
@@ -4,10 +4,43 @@
  */
 package board;
 
+import java.io.IOException;
+import utils.CSVReader;
+
 /**
  *
- * @author AltAWKEl
+ * @author Nour
  */
 public class SudokuBoard {
+    private int[][]board;
+    
+    public SudokuBoard()
+    {board=new int[9][9];
+    board=null;}
+    
+    public void setBoard(int[][]board)
+    {for(int i=0;i<9;i++)
+        for(int j=0;j<9;j++)
+            if(board[i][j]<0 || board[i][j]>9)
+                throw new IllegalArgumentException("Invalid number entered.");
+    this.board=board;
+    
+    }
+    
+    public int[][] getBoard()
+    {return this.board;}
+    
+    public void importBoardFromFile(String filename)
+    {  int[][]board=new int[9][9];
+        try{
+       board=CSVReader.readCSV(filename);
+       setBoard(board);
+    }
+    catch(IOException e)
+    {System.out.println("Error in reading file.");}
+    catch(IllegalArgumentException e)
+    {System.out.println("Try a different file or edit the file.");}
+    
+    }
     
 }
