1ada22735818d05036bb35168fe4459a9ffbd001
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-11-29 13:35:40 +0200

    Add files via upload
    factory method for choosing the validator mode

--- src/factory/ValidatorFactory.java
+++ src/factory/ValidatorFactory.java
@@ -4,10 +4,30 @@
  */
 package factory;
 
+import modes.*;
+
 /**
  *
- * @author AltAWKEl
+ * @author Nour
  */
 public class ValidatorFactory {
-    
+
+    public ModeValidator getType(int mode) {
+        switch (mode) {
+            case 0 -> {
+                return new Mode0Validator();
+            }
+            case 3 -> {
+                return new Mode3Validator();
+            }
+            case 27 -> {
+                return new Mode27Validator();
+            }
+            default -> {
+                System.out.println("Invalid mode entered.");
+                return null;
+            }
+        }
+
+    }
 }
