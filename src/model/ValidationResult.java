c5c487b9b2213a01993aa5460216ac06aa9d2cc4
moaz91 <134746907+moaz91@users.noreply.github.com>
2025-11-30 12:49:58 +0200

    Add files via upload

--- src/model/ValidationResult.java
+++ src/model/ValidationResult.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author AltAWKEl
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidationResult {
-    
+    private boolean isValid;
+    private List<String> errors;
+
+    public ValidationResult() {
+        this.isValid = true;
+        this.errors = new ArrayList<>();
+    }
+
+    public ValidationResult(boolean isValid, List<String> errors) {
+        this.isValid = isValid;
+        this.errors = errors;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
+    }
+
+    public void addError(String error) {
+        this.errors.add(error);
+        this.isValid = false;
+    }
+
+    public void printResult() {
+        if (isValid) {
+            System.out.println("VALID");
+        } else {
+            System.out.println("INVALID");
+            for (String error : errors) {
+                System.out.println(error);
+            }
+        }
+    }
 }
